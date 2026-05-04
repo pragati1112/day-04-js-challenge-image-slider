4ed9d2a99b0fec9b5c521725762ccd56b7df6471
pragati makwana <ajayahir3038@gmail.com>
2026-05-04 17:48:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let images = [
+  "https://picsum.photos/id/1015/600/300",
+  "https://picsum.photos/id/1016/600/300",
+  "https://picsum.photos/id/1018/600/300"
+];
+
+let index = 0;
+
+function showImage() {
+  document.getElementById("slide").src = images[index];
+}
+
+function next() {
+  index = (index + 1) % images.length;
+  showImage();
+}
+
+function prev() {
+  index = (index - 1 + images.length) % images.length;
+  showImage();
+}
+
+// Initialize the first image on load
+showImage();
